2d4e614a616156de9c9dfe4fc94c132785482534
romashaa <mariia.romanenko@ukma.edu.ua>
2024-04-22 11:48:20 +0300

    Task1 done

--- HW1_dotnet_itvdn/Program.cs
+++ HW1_dotnet_itvdn/Program.cs
@@ -10,7 +10,16 @@ namespace HW1_dotnet_itvdn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Сторона а:");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Сторона b:");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter}");
+            Console.WriteLine($"Площа прямокутника: {rectangle.Area}");
 
             Console.ReadKey();
         }
